40ff67fd66b4884c72d0129b23e00252e5fcf2ba
zhouxuming <zhouxuming@rongcloud.cn>
2019-10-15 18:25:31 +0800

    处理签名问题

--- library/src/main/java/com/comjia/push/library/platform/hms/HMSReceiver.java
+++ library/src/main/java/com/comjia/push/library/platform/hms/HMSReceiver.java
@@ -2,6 +2,7 @@ package com.comjia.push.library.platform.hms;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.huawei.hms.support.api.push.PushReceiver;
 
@@ -10,16 +11,33 @@ public class HMSReceiver extends PushReceiver {
 
     @Override
     public void onToken(Context context, String token, Bundle bundle) {
+        Log.e(TAG, "token: " + token);
     }
 
     @Override
     public boolean onPushMsg(Context context, byte[] msg, Bundle bundle) {
-
+        Log.d(TAG, "onPushMsg");//透传接收消息的地方 杀死进程华为手机也收不到不建议使用
+        try {
+            String content = new String(msg, "UTF-8");
+            Log.d(TAG, "onPushMsg content : " + content);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return false;
     }
 
     @Override
     public void onPushState(Context context, boolean pushState) {
+        Log.e(TAG, "The current push status： " + (pushState ? "Connected" : "Disconnected"));
+    }
+
+    @Override
+    public void onEvent(Context context, Event event, Bundle extras) {
+        if (Event.NOTIFICATION_OPENED.equals(event)) {
+
+        } else if (Event.NOTIFICATION_CLICK_BTN.equals(event)) {
+
+        }
     }
 }
