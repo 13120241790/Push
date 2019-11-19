af93ffddf83f2f701af926aae3e675ff3c2ce85e
zhouxuming <zhouxuming@rongcloud.cn>
2019-11-19 09:49:42 +0800

    delete v7

--- library/src/main/java/com/comjia/push/library/platform/hms/NotifyDetailActivity.java
+++ library/src/main/java/com/comjia/push/library/platform/hms/NotifyDetailActivity.java
@@ -2,11 +2,10 @@ package com.comjia.push.library.platform.hms;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 
 public class NotifyDetailActivity extends Activity {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 }
